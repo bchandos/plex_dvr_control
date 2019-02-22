1a60d4da986e6812743a7fc6c870bea8835637f1
Bill Chandos <bill.chandos@gmail.com>
2019-02-21 20:57:56 -0800

    Fixing class constructor issues and scoping

--- plex_api.py
+++ plex_api.py
@@ -1,16 +1,25 @@
 import requests
 import xml.etree.ElementTree as ET
 
+from urllib import parse
+
 
 class PlexServer:
+    def __init__(self, host, token, client_identifier, port='32400'):
+        self.library = _PlexLibrary(host=host, token=token, port=port)
+        self.dvr = _PlexDVR(host=host,
+                            token=token,
+                            client_identifier=client_identifier,
+                            port=port)
+
+
+class _PlexLibrary:
+
     def __init__(self, host, token, port='32400'):
         self.base_url = f'http://{host}:{port}'
+        # self.dvr_base_url = self.base_url + '/tv.plex.providers.epg.onconnect:2'
         self.parameters = {'X-Plex-Token': token}
-        self.library = PlexLibrary(host=host, token=token, port=port)
-        self.dvr = PlexDVR(host=host, token=token, port=port)
-
-
-class PlexLibrary(PlexServer):
+        self.token = token
 
     def all_shows(self, library_id):
         """
@@ -129,13 +138,108 @@ class PlexLibrary(PlexServer):
         return d
 
 
-class PlexDVR(PlexServer):
+class _PlexDVR:
+
+    def __init__(self, host, token, client_identifier, port='32400'):
+        self.base_url = f'http://{host}:{port}'
+        self.dvr_base_url = self.base_url + '/tv.plex.providers.epg.onconnect:2'
+        self.parameters = {'X-Plex-Token': token}
+        self.token = token
+        self.client_identifier = client_identifier
 
     def subscriptions(self):
-        pass
+        r = requests.get(
+            f'{self.base_url}/media/subscriptions', params=self.parameters)
+        sub_tree = ET.fromstring(r.text)
+        sub_list = list()
+        for sub in sub_tree:
+            for directory in sub:
+                sub_list.append(sub.attrib + directory.attrib)
+        return sub_list
 
-    def shows(self, show_gracenote_id):
-        pass
+    def shows(self):
+        # /tv.plex.providers.epg.onconnect:2/sections/2/all
+        r = requests.get(
+            f'{self.dvr_base_url}/sections/2/all', params=self.parameters)
+        show_tree = ET.fromstring(r.text)
+        show_list = list()
+        for show in show_tree:
+            show_list.append(show.attrib)
+        return show_list
 
     def seasons(self, show_gracenote_id=None, show_url=None):
+        # com.gracenote.onconnect://show/{show_gracenote_id}
+        # /tv.plex.providers.epg.onconnect:2/metadata/com%2Egracenote%2Eonconnect%3A%2F%2Fshow%2F184483/children
         pass
+
+    def episodes(self, show_gracenote_id=None, season=None, season_url=None):
+        # com.gracenote.onconnect://season/{show_gracenote_id}/{season}
+        # /tv.plex.providers.epg.onconnect:2/metadata/com%2Egracenote%2Eonconnect%3A%2F%2Fseason%2F184483%2F6/children
+        pass
+
+    def episode(self, episode_gracenote_id=None, episode_url=None):
+        # com.gracenote.onconnect://episode/EP############
+        # /tv.plex.providers.epg.onconnect:2/metadata/com%2Egracenote%2Eonconnect%3A%2F%2Fepisode%2FEP002960010113
+        # not sure this is necessary as most, if not all, data is available in the season listing page
+        pass
+
+    def set_recording(self, episode, year):
+        """
+        Prepares and sends the Plex POST request to add a recording to the Plex DVR.
+        :param episode: The episode object representing the guide XML data for an episode
+        :param year: the year the show began; only available in the root element of a season
+        :returns: True if succesful, False if failed
+        :rtype: bool
+        """
+
+        parameters_prefs = {
+            'prefs[minVideoQuality]': '0',
+            'prefs[replaceLowerQuality]': 'false',
+            'prefs[recordPartials]': 'false',
+            'prefs[startOffsetMinutes]': '0',
+            'prefs[endOffsetMinutes]': '0',
+            'prefs[lineupChannel]': '',
+            'prefs[startTimeslot]': '-1',
+            'prefs[comskipEnabled]': '1',
+            'prefs[oneShot]': 'true',
+            'targetLibrarySectionID': '3',
+            'targetSectionLocationID': '',
+            'includeGrabs': '1'}
+        parameters_brackets = {
+            'hints[grandparentGuid]': episode['grandparentGuid'],
+            'hints[grandparentThumb]': episode['grandparentThumb'],
+            'hints[grandparentTitle]': episode['grandparentTitle'],
+            'hints[grandparentYear]': year,
+            'hints[guid]': episode['guid'],
+            'hints[index]': episode['index'],
+            'hints[originallyAvailableAt]': episode['originallyAvailableAt'][:10],
+            'hints[parentIndex]': episode['parentIndex'],
+            'hints[title]': episode['title'],
+            'hints[type]': '4',
+            'hints[year]': episode['year'],
+            'params[airingChannels]': parse.quote(episode[0]['channelIdentifier']+'='+episode[0]['channelTitle']),
+            'params[airingTimes]': int(
+                ((int(episode['originallyAvailableAt'][11:13]) + 8) +
+                 int(episode['originallyAvailableAt'][14:16]) / 60) * 60) % 1440,
+            'params[libraryType]': '2',
+            'params[mediaProviderID]': '3',
+            'type': '4',
+            'X-Plex-Product': 'Plex Web',
+            'X-Plex-Version': '3.77.4',
+            'X-Plex-Client-Identifier': self.client_identifier,
+            'X-Plex-Platform': 'Chrome',
+            'X-Plex-Platform-Version': '67.0',
+            'X-Plex-Sync-Version': '2',
+            'X-Plex-Device': 'Windows',
+            'X-Plex-Device-Name': 'Chrome',
+            'X-Plex-Device-Screen-Resolution': '1920x1080',
+            'X-Plex-Token': self.token,
+            'X-Plex-Language': 'en'
+        }
+
+        parameters = f'{parse.urlencode(parameters_prefs, quote_via=parse.quote)}&' \
+            f'{parse.urlencode(parameters_brackets, safe="[]", quote_via=parse.quote)}'
+
+        url = f'{self.base_url}/media/subscriptions?{parameters}'
+        p = requests.post(url)
+        return p.ok
